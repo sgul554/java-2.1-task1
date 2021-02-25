014bae65f5ff4493935fee5e94cd60459e588cf3
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-02-25 17:54:46 +0300

    Добавление файлов

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 10295;
+        int amount = 20;
+        int miles = price / amount;
+        System.out.println(miles);
+
+    }
+}
